fd8e62ffefd998ba899bca38c0dbe868655f2732
takashiki <857995137@qq.com>
2016-07-29 19:17:13 +0800

    小修改

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,22 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return 'hi';
-});
-
 Route::post('/upload', 'MainController@upload');
 
 Route::any('/{sha1}', 'MainController@view')->where(['sha1' => '\w+']);
-
-
